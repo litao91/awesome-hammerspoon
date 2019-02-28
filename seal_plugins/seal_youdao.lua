200a256e6d5119d9cda63c06873054fd1559aa0f
Tao LI <litao.91@bytedance.com>
2019-02-28 11:05:28 +0800

    Youdao works for seal

--- seal_plugins/seal_youdao.lua
+++ seal_plugins/seal_youdao.lua
@@ -1,15 +1,15 @@
 local obj = {}
 obj.__index = obj
-obj.__name = "seal_yd"
+obj.__name = "seal_youdao"
 
 function obj:bare()
   return nil
 end
 
 function obj:commands()
-  return {yd = {
+  return {yd= {
       cmd="yd",
-      fn = obj.youdaoInstantTrans,
+      fn = obj.runYoudao,
       name = "Youdao Dict",
       description = "Translate with youdao dict",
       plugin = obj.__name
@@ -17,37 +17,57 @@ function obj:commands()
   }
 end
 
+local function basic_extract(arg)
+    if arg then return arg.explains else return {} end
+end
+local function web_extract(arg)
+    if arg then
+        local value = hs.fnutils.imap(arg, function(item)
+            return item.key .. table.concat(item.value, ",")
+        end)
+        return value
+    else
+        return {}
+    end
+end
 
-function obj:youdaoInstantTrans(querystr)
-    local youdao_keyfrom = 'hsearch'
-    local youdao_apikey = '1199732752'
-    local youdao_baseurl = 'http://fanyi.youdao.com/openapi.do?keyfrom=' .. youdao_keyfrom .. '&key=' .. youdao_apikey .. '&type=data&doctype=json&version=1.1&q='
+function obj.runYoudao(query)
+  local youdao_keyfrom = 'hsearch'
+  local youdao_apikey = '1199732752'
+  local youdao_baseurl = 'http://fanyi.youdao.com/openapi.do?keyfrom=' .. youdao_keyfrom .. '&key=' .. youdao_apikey .. '&type=data&doctype=json&version=1.1&q='
+
   if query == nil or query == "" or query == ".*" then
-      local chooser_data = {
-          {text="Youdao Dictionary", subText="Type something to get it translated …"}
-      }
-      return chooser_data
+    local chooser_data = {
+        {text="Youdao Dictionary", subText="Type something to get it translated …"}
+    }
+    return chooser_data
   else
-        local encoded_query = hs.http.encodeForQuery(querystr)
-        local query_url = youdao_baseurl .. encoded_query
+    local encoded_query = hs.http.encodeForQuery(query)
+    local query_url = youdao_baseurl .. encoded_query
+    print("youdao query: " .. query_url)
 
-        status, data = hs.http.get(query_url, "")
-        if status == 200 then
-            if pcall(function() hs.json.decode(data) end) then
-                local decoded_data = hs.json.decode(data)
-                if decoded_data.errorCode == 0 then
-                    local basictrans = basic_extract(decoded_data.basic)
-                    local webtrans = web_extract(decoded_data.web)
-                    local dictpool = hs.fnutils.concat(basictrans, webtrans)
-                    if #dictpool > 0 then
-                        local chooser_data = hs.fnutils.imap(dictpool, function(item)
-                            return {text=item, output="clipboard", arg=item}
-                        end)
-                        return chooser_data
-                    end
-                end
-            end
+    status, data = hs.http.get(query_url, nil)
+    print(data)
+    if status == 200 then
+      local decoded_data = hs.json.decode(data)
+      if decoded_data.errorCode == 0 then
+        local basictrans = basic_extract(decoded_data.basic)
+        local webtrans = web_extract(decoded_data.web)
+        local dictpool = hs.fnutils.concat(basictrans, webtrans)
+        if #dictpool > 0 then
+          local chooser_data = hs.fnutils.imap(dictpool, function(item)
+            return {text=item, output="clipboard", arg=item}
+          end)
+          return chooser_data
         end
+      end
+    end
+  end
+end
+
+function obj.completionCallback(rowInfo)
+    if rowInfo["type"] == "copyToClipboard" then
+        hs.pasteboard.setContents(rowInfo["text"])
     end
 end
 
